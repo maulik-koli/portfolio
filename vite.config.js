8e523fe44f57068d33e9a1766adc72ecdbb5d241
maulik-koli <maulikkoli1021@gmail.com>
2024-11-14 19:35:50 +0530

    Set base url for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = "/maulik-koli/portfolio";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repoName,
   plugins: [react()],
 })
