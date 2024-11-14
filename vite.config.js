980f2bff7a2f5416370f30a7bc5e94fc9b022a39
maulik-koli <maulikkoli1021@gmail.com>
2024-11-14 19:45:38 +0530

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = "/maulik-koli/portfolio";
+const repoName = "/portfolio";
 
 // https://vite.dev/config/
 export default defineConfig({
